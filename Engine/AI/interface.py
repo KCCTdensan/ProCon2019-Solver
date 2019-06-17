e8cfd37327fdb6f1dde48215bf3035399a54ff8a
kamejiro <r116309@g.kobe-kosen.ac.jp>
2019-06-17 14:47:36 +0900

    first commit

---
+++ Engine/AI/interface.py
@@ -0,0 +1,18 @@
+class Interface:
+	def initialize():
+		pass
+
+	def finalize():
+		pass
+
+	def setBoard():
+		pass
+
+	def setPosition():
+		pass
+
+	def think():
+		pass
+
+	def getBestMove():
+		pass
