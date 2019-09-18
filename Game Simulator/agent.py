52a9ef6ccda3a90d376aa9fec82792915ef9200a
nekosaburo8250 <r116309@g.kobe-kosen.ac.jp>
2019-09-18 11:01:08 +0900

    update

--- Game Simulator/agent.py
+++ Game Simulator/agent.py
@@ -4,13 +4,19 @@ from ..Common.basic import Position
 from ..Common.basic import Direction
 
 class Agent:
-	def __init__(self, position:Position):
+	def __init__(self, field:Field, position:Position):
+		self.__field = field
 		self.__position = position
-		return
-
-	def move(self, direction:Direction):
-		self.__position.move(direction)
 		return
 
 	def getPosition(self)->Position:
 		return self.__position
+
+	def canMove(direction:Direction)->bool:
+		nextPosition = self.__position.copy()
+		nextPosition.move(direction)
+		return (nextPosition.x >= 0) and (nextPosition.x < self.__field.getSize().x) and (nextPosition.y >= 0) and (nextPosition.y < self.__field.getSize().y)
+	
+	def move(self, direction:Direction):
+		self.__position.move(direction)
+		return
---
+++ Game Simulator/agent_manager.py
@@ -0,0 +1,8 @@
+﻿from agent import Agent
+from ..Common.field import Field
+
+
+class AgentManager:
+	def __init__(self, field:Field, numAgents:int):
+		pass
+
